b912701e651f723faa7742ffab83debc808b9f77
GandyA23 <avila.gandy@gmail.com>
2021-09-30 15:51:54 -0500

    Lectura con consola, concatenación, raw string y template string en Kotlin

---
+++ src/main/kotlin/InConcatTempRawStr.kt
@@ -0,0 +1,25 @@
+fun main () {
+    // Lectura desde consola con el método readLine
+    print("Ingresa tu nombre: ")
+    val nombre = readLine()
+
+    print("Ingresa tu edad: ")
+    val edad = readLine()
+
+    // Concatenación de cadenas
+    val mensaje = "Tu nombre es " + nombre + " y tu edad es " + edad + " años"
+
+    // Template string
+    val templateString = "Tu nombre es $nombre y tu edad es $edad años"
+
+    // Raw String da un formato a como lo mostrará en consola
+    val rawString = """
+        *** Datos ***
+        Nombre: $nombre
+        Edad: $edad años
+    """.trimIndent()
+
+    println(mensaje)
+    println(templateString)
+    println(rawString)
+}
